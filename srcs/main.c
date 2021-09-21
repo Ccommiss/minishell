012a570f69c4529f9e3f1e0b8e644d847a2ff54f
Melanie POCHARD <mpochard@e1r11p11.42.fr>
2021-09-21 17:33:13 +0200

    control quote ok + je travail sur le split espace sauf quote

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mpochard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 16:46:53 by mpochard          #+#    #+#             */
-/*   Updated: 2021/09/20 10:45:34 by mpochard         ###   ########.fr       */
+/*   Updated: 2021/09/21 13:43:31 by mpochard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,71 +15,60 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int	init_check_op(int *i, int *count, int *count1, int *pipe)
-{
-	int	nb_redir;
-
-	nb_redir = 0;
-	*i = 0;
-	*count = 0;
-	*count1 = 0;
-	*pipe = 0;
-	return (nb_redir);
-}
-
-int	check_op(char *line, t_arg *op)
+int	check_quote(char *line)
 {
 	int	i;
-	int	count;
-	int	count1;
-	int	pipe;
-	int	nb_redir;
+	int check;
 
-	nb_redir = init_check_op(&i, &count, &count1, &pipe);
+	i = 0;
 	while (line[i])
 	{
+		check = 0;
 		if (line[i] == '\'')
-			count++;
+		{
+			check = 1;
+			i++;
+			while (line[i]  && line[i] != '\'')
+				i++;
+			if (line[i] && line[i] == '\'')
+				check = 2;
+		}
 		if (line[i] == '\"')
-			count1++;
-		if (line[i] == '|')
-			pipe++;
-		if (line[i] == '<' || line[i] == '>')
-			nb_redir++;
+		{
+			check = 1;
+			i++;
+			while (line[i]  && line[i] != '\"')
+				i++;
+			if (line[i] && line[i] == '\"')
+				check = 2;
+		}
 		i++;
+		
 	}
-	if (count % 2 == 1 || count1 % 2 == 1)
-		return (printf("quote are not closed\n"));
-	op->d_quote = count1;
-	op->s_quote = count;
-	op->nb_pipe = pipe;
+		if (check == 1)
+			return (-1);
 	return (0);
 }
-/* check and slit with the pipe"*/
 
 int	main(int ac, char **av, char **env)
 {
 	char	*line;
-	t_arg	op;
 
 	(void)ac;
 	(void)av;
 	(void)env;
 	while (1)
 	{
-		ft_bzero(&op, sizeof(op));
 		line = readline("> ");
 		if (line)
 			add_history(line);
-		if (check_op(line, &op) > 1)// FREE LINE 
-			continue ;
-		if (ft_split_cmd(line, '|', op) == NULL)
-
-		if (ft_strncmp(line, "exit", 5) == 0)
+		if (check_quote(line) ==-1)
 		{
 			free(line);
-			return (0);
+			printf("quote are not closed\n");
+			continue;
 		}
+	free(line);
 	}
 	return (0);
 }
--- srcs/split_arg.c
+++ srcs/split_arg.c
@@ -6,27 +6,23 @@
 /*   By: mpochard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 13:25:57 by mpochard          #+#    #+#             */
-/*   Updated: 2021/09/20 13:06:05 by mpochard         ###   ########.fr       */
+/*   Updated: 2021/09/21 17:31:25 by mpochard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 /*
- * this fonction split the arg of line with a sep like | (readline) 
- * ex : " ls -l | wc > a
- * tab[0] = ls -l
- * tab[1] = |
- * tab[2] = wc > a
+ * this fonction split the arg of line  with space \t \v \r  (readline) 
  * */
 
-int	ft_sep(char c, char sep)
+int	ft_sep(char c)
 {
-	if ( c == sep)
+	if (ft_isin(c," \n\v\f\r") == 1)
 		return (1);
 	return (0);
 }
 
-int	ft_count_sep(char *str, char sep)
+int	ft_count_word(char *str)
 {
 	int	i;
 	int	count;
@@ -35,16 +31,43 @@ int	ft_count_sep(char *str, char sep)
 	count = 0;
 	while (str[i])
 	{
-		if ( ft_sep(str[i], sep) == 1)
-			if ( i != 0 && ft_sep(str[i - 1], sep) == 0)
+			if ( i == 0 && (str[i] == '\"'))
+			{
+				i++;
+				while (str[i] && str[i] != '\"')
+				{
+					i++;
+				}
+					if ( str[i] == '\"')
+				{
+					count++;
+				}
+			}
+		if ( i && ft_sep(str[i - 1]) == 1)
+		{
+			if (str[i] == '\'' || str[i] == '\"')
+			{
+				i++;
+				while (str[i] && (str[i] != '\'' || str[i] != '\"'))
+					i++;
+				if (str[i] == '\'' || str[i] == '\"')
+				{
+					i++;
+					printf("123\n");
+					count++;
+				}
+			}
+			}
+		if ( ft_sep(str[i]) == 1)
+			if ( i != 0 && ft_sep(str[i - 1]) == 0)
 				count++;
 		i++;
 	}
-	if (ft_sep(str[i], sep) == 0)
+	if (str[i - 1] != '\"' && ft_sep(str[i -1]) == 0)
 		count++;
 	return (count);
 }
-
+/*
 char		**free_malloc(char **tab)
 {
 	int i;
@@ -66,12 +89,12 @@ char		*ft_strldup(char *str, char c)
 	int		count;
 
 	count = 0;
-	while (str[count] && ft_sep(str[count], c) == 0)
+	while (str[count] && ft_sep(str[count]) == 0)
 		count++;
 	if (!(fi = malloc(sizeof(char) * (count + 1))))
 		return (NULL);
 	i = 0;
-	while (str[i] && ft_sep(str[i], c) == 0)
+	while (str[i] && ft_sep(str[i]) == 0)
 	{
 		fi[i] = str[i];
 		i++;
@@ -81,7 +104,7 @@ char		*ft_strldup(char *str, char c)
 }
 
 // NEED TO RECODE STRNDUp
-char	**ft_split_cmd(char *str, char sep, int nb)
+char	**ft_split_cmd(char *str)
 {
 	char **tab;
 	int	i;
@@ -90,35 +113,32 @@ char	**ft_split_cmd(char *str, char sep, int nb)
 
 	i = 0;
 	j = 0;
-	nbr = ft_count_sep(str, sep) + nb;
+	nbr = ft_count_sep(str);
 	tab = malloc(sizeof(char *) * (nbr + 1));
 	if (tab == NULL)
 		return (NULL);
 	while ( j < nbr && str[i])
 	{
-		while (str[i] && (ft_sep(str[i], sep) == 1))
-		{
-			tab[j++] = strndup(&str[i], 1);
-			i++;
-		}
-		if (!(tab[j++] = ft_strldup(&str[i], sep)))
-			return (free_malloc(tab));
-		while (str[i] && (ft_sep(str[i], sep) == 0))
-			i++;
+
 	}
 	tab[j] = 0;
 	return (tab);
 }
-
+*/
 int main (int ac, char **av)
 {
 	(void)ac;
-	char **test = ft_split_cmd(av[1], '|', 1);
+	(void)av;
+	char	*line;
+
+	line = readline("hi>");
+	printf("i %d\n", ft_count_word(line));
+/*	char **test = ft_split_cmd(av[1]);
 	int i = 0;
 
 	while ( test[i])
 	{
 		printf (" test %s\n",test[i]);
 		i++;
-	}
+	}*/
 }
