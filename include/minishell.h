db7eab9cdef8809e3dda8f69f5f3a848e409a595
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2021-09-23 10:08:17 +0200

    modification minishell.h pour ajout de structs + tokenisation

--- include/minishell.h
+++ include/minishell.h
@@ -1,18 +1,70 @@
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "../libft/libft.h"
 
-typedef struct	s_arg
+typedef struct s_arg
 {
-	int	s_quote;
-	int	d_quote;
-	int	nb_pipe;
-}				t_arg;
+	int s_quote;
+	int d_quote;
+	int nb_pipe;
+} t_arg;
+
+
+enum bool
+{
+	ERROR = -1,
+	FALSE,
+	TRUE
+};
+enum tokens
+{
+	OP = 0,
+	WORD = 1,
+	TOK_WORD = 100,
+	TOK_PIPE,
+	TOK_GREAT,
+	TOK_LESS,
+	SQUOTE = '\'',
+	DQUOTE = '\"'
+};
+
+typedef struct s_token t_token;
+typedef struct s_token
+{
+	char *content;
+	int type;
+	int index;
+	int len;
+	int to_expand;
+	t_token *next;
+	t_token *prev;
+} t_token;
+
+typedef struct s_cmd t_cmd;
+typedef struct s_cmd
+{
+	int index;
+	char *cmdp; //le path
+	char **cmd_args;
+	t_cmd *next;
+	t_cmd *head;
+
+} t_cmd;
+
+
+typedef struct s_data
+{
+	char ***envp;  // stocke envuronnemebnt
+	t_token *toks; // ptr sur la head de la liste chainee des tokens
+	t_cmd *cmds;   // ptr sur la head de la liste chainee des commandes
+
+} t_data;
+
 #endif
