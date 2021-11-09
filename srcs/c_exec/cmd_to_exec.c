650944198d55a682f5c5bd59670645a67323ef64
Claire COMMISSAIRE <claire.commissaire@sciencespo.fr>
2021-11-09 10:39:16 +0100

    fixing little error for return value (set at 0 in cmd to exc only if no error in the cmd)

--- srcs/c_exec/cmd_to_exec.c
+++ srcs/c_exec/cmd_to_exec.c
@@ -6,7 +6,7 @@
 /*   By: ccommiss <ccommiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 10:05:27 by mpochard          #+#    #+#             */
-/*   Updated: 2021/11/08 14:59:18 by mpochard         ###   ########.fr       */
+/*   Updated: 2021/11/09 10:18:40 by ccommiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,11 @@ void	cmd_to_exec(t_cmd *cmd, t_env *env)
 	char *buf;
 	char	**tenvp;
 
-	return_value = 0;
+	//return_value = 0;
 	while (cmd)
 	{
+		if (cmd->error == FALSE)
+			return_value = 0;
 		if (cmd->next)
 		{
 			if (do_the_pipe(cmd, env) == 0)
